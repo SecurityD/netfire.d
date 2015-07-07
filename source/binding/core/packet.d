bcbb191fc122a59ec780a79ad63102689f97aca6
Matthieu Kern <matthieu.kern@epitech.eu>
2015-07-07 23:02:37 +0200

    Add data to protocol

--- source/binding/core/packet.d
+++ source/binding/core/packet.d
@@ -3,13 +3,16 @@ module binding.core.packet;
 import core.vararg;
 import std.string;
 import std.conv;
+import std.datetime;
 
 import ruby;
 
 import netload.core.packet;
+import netload.core.protocol;
 
 extern(C) struct rb_Packet {
   Packet packet;
+  VALUE rbPacketPtr;
 
   static VALUE getAnnotation(VALUE self, ...) {
     rb_Packet* ptr = Data_Get_Struct!rb_Packet(self);
@@ -25,7 +28,12 @@ extern(C) struct rb_Packet {
 
   static VALUE getTime(VALUE self, ...) {
     rb_Packet* ptr = Data_Get_Struct!rb_Packet(self);
-    return rb_str_new_cstr(ptr.packet.time.toString.toStringz);
+    return rb_str_new_cstr(ptr.packet.time.toSimpleString.toStringz);
+  }
+
+  static VALUE getData(VALUE self, ...) {
+    rb_Packet* ptr = Data_Get_Struct!rb_Packet(self);
+    return ptr.rbPacketPtr;
   }
 
   static void free(void* p) {
@@ -33,7 +41,22 @@ extern(C) struct rb_Packet {
   }
 
   static VALUE initialize(int ac, VALUE* av, VALUE self) {
+    struct getProtocol {
+      Protocol p;
+    }
+
     rb_Packet* ptr = Data_Get_Struct!rb_Packet(self);
+
+    VALUE rbPacket, rbTime = Qnil;
+    rb_scan_args(ac, av, "11", &rbPacket, &rbTime);
+    getProtocol* pr = Data_Get_Struct!getProtocol(rbPacket);
+    if (rbTime != Qnil) {
+      ptr.packet = new Packet(pr.p, SysTime.fromSimpleString(cast(string)(rb_string_value_cstr(&rbTime).fromStringz)));
+    } else {
+      ptr.packet = new Packet(pr.p);
+    }
+    ptr.rbPacketPtr = rbPacket;
+
     return self;
   }
 
@@ -52,5 +75,6 @@ extern(C) struct rb_Packet {
     rb_define_method(singInst, "annotation".toStringz, &getAnnotation, 0);
     rb_define_method(singInst, "annotation=".toStringz, &setAnnotation, 1);
     rb_define_method(singInst, "time".toStringz, &getTime, 0);
+    rb_define_method(singInst, "data".toStringz, &getData, 0);
   }
 }
